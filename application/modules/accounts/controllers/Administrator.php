508fc2c3f2952f9d0734f63dcef006836deb62ef
azil-develop <allsave101@gmail.com>
2020-01-10 00:00:19 +0700

    Menambah Title Pada halaman Login

--- application/modules/accounts/controllers/Administrator.php
+++ application/modules/accounts/controllers/Administrator.php
@@ -12,8 +12,8 @@ class Administrator extends Backend_Controller {
 	}
 	public function index()
 	{
-		
-		$this->load->view('accounts/v_home');
+		$data['title'] = "Halaman Login";		
+		$this->load->view('accounts/v_home',$data);
 		
 	}
 }
---
+++ application/modules/accounts/controllers/Register.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('Halaman ini Tidak Bisa di Akses');
+
+/**
+ * 
+ */
+class Register extends Backend_Controller
+{
+	
+	function __construct()
+	{
+		# code...
+	}
+
+	public function index()
+	{
+
+	}
+
+	public function checkRegister()
+	{
+
+	}
+}
