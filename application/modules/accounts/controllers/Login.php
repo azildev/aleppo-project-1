edb2188f54c237b75b3ba0530ea7451508238c1e
azil-develop <allsave101@gmail.com>
2020-01-10 01:23:33 +0700

    Terserah Aja

--- application/modules/accounts/controllers/Login.php
+++ application/modules/accounts/controllers/Login.php
@@ -18,7 +18,7 @@ class Login extends Backend_Controller
 	{
 		$data['title'] = "Halaman Login 1";	
 		$data['beranda'] = "Beranda Halaman Login";
-		$data['tes'] = "terserah dah";		
+		$data['tes'] = "terserah";		
 		$this->load->view('accounts/v_login',$data);
 	}
 
