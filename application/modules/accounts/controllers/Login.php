124ac8aa75d7b194ae0cd7f1bd399ee85afbcfe5
arizal7 <arizal925@gmail.com>
2020-01-10 01:18:29 +0700

    perubahan pada tes

--- application/modules/accounts/controllers/Login.php
+++ application/modules/accounts/controllers/Login.php
@@ -18,7 +18,7 @@ class Login extends Backend_Controller
 	{
 		$data['title'] = "Halaman Login 1";	
 		$data['beranda'] = "Beranda Halaman Login";
-		$data['tes'] = "Mau Pakai APa Saja Ya";		
+		$data['tes'] = "terserah";		
 		$this->load->view('accounts/v_login',$data);
 	}
 
